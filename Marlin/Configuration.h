20536480abbf533ef66ab8b0f1716b4ed06a1cb3
scodari <ste.coda@gmail.com>
2021-12-23 20:48:44 +0100

    Custom PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -602,9 +602,9 @@
     #define DEFAULT_Kd_LIST {  78.81,  78.81 }
   #else
     // Ender 3 v2
-    #define DEFAULT_Kp  28.72
-    #define DEFAULT_Ki   2.62
-    #define DEFAULT_Kd  78.81
+    #define DEFAULT_Kp  30.80
+    #define DEFAULT_Ki   3.11
+    #define DEFAULT_Kd  76.22
   #endif
 #endif // PIDTEMP
 
@@ -642,9 +642,9 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   // Ender 3 V2
-  #define DEFAULT_bedKp 462.10
-  #define DEFAULT_bedKi  85.47
-  #define DEFAULT_bedKd 624.59
+  #define DEFAULT_bedKp 241.55
+  #define DEFAULT_bedKi  46.36
+  #define DEFAULT_bedKd 839.08
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
